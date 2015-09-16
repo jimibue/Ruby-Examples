52fa4980693573423fd6caf6583fa11960425a86
James Yeates <jimibue@gmail.com>
2015-09-16 17:18:42 -0600

    added chp5 directory

---
+++ chp5-inher-modules-mixins/inheritance.rb
@@ -0,0 +1,28 @@
+#Basic Object is Base Class of RUBY
+p Object.superclass       #  BasicObject
+p BasicObject.superclass  # "nil"
+
+class Animal
+  def initialize(age, weight)
+    @age = age
+    @weight = weight
+  end
+  def details
+    "age: #{@age} weight:#{@weight}"
+  end
+end
+
+class Dog < Animal
+  def initialize(age, weight, name)
+    super(age, weight)
+    @name = name
+  end
+  def details
+    "I am #{@name} and I am " + super
+  end
+end
+
+d = Dog.new(21,100,"grant")
+p d.details # "I am grant and I am age: 21 weight:100"
+
+f

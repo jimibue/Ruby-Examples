4bfcda0f6667c6d7a343411f7d28ca2249bdfaaa
James Yeates <jimibue@gmail.com>
2015-09-17 12:47:14 -0600

    added simple singelton example

--- chp5-inher-modules-mixins/singleton_method.rb
+++ chp5-inher-modules-mixins/singleton_method.rb
@@ -13,9 +13,10 @@ dog = Animal.new('bark')
 parrot = Animal.new('chirp')
 
 ##only parrots can say words so we'll have it get its own singleton method
-Hungry = true;
+HUNGRY = true
 def parrot.talk
-  if Hungry then
+  if HUNGRY
+  then
      puts "Polly want a cracker"
   else
     speak
@@ -26,8 +27,22 @@ parrot.talk #Polly want a cracker
 
 #check to see if a singleton method exist
 p parrot.singleton_methods
-if parrot.singleton_methods.include?(:talk) then
+if parrot.singleton_methods.include?(:talk)
+then
   puts "yes I have"
+  parrot.talk
 else
   puts "don't have"
 end
+
+##singleton classes is a class that defines a single object
+obj = Object.new
+
+##anonymous class
+class<<obj
+  def a_method
+    puts "yo!"
+  end
+end
+
+obj.a_method()

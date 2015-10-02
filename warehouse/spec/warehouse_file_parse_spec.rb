31d16afec3394b21db173636114d65f033ac826b
James Yeates <jimibue@gmail.com>
2015-10-02 11:59:15 -0600

    renamed file

---
+++ warehouse/spec/warehouse_file_parse_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require_relative 'spec/spec_helper'
+require_relative 'warehouse_file_parse_spec'
+
+describe 'WarehouseFileParser' do
+
+ describe 'When working with' do
+   describe '' do
+   end
+ end
+end

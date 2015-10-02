10fd4a440c68537a42b1193d7928d666c8f0149f
James Yeates <jimibue@gmail.com>
2015-10-02 12:55:12 -0600

    added intialze method and process_file method

--- warehouse/warehouse_file_parser.rb
+++ warehouse/warehouse_file_parser.rb
@@ -1,6 +1,34 @@
 class WarehouseFileParser
+  attr_reader :file
 
-  def init(file)
-    @file = File.open(file,'r')
+  def initialize(file)
+    begin
+      @file = File.open(file,'r')
+      process_file(@file)
+    rescue
+      @file ="Could not open file"
+    end
+  end
+
+  def process_file(file)
+   #parse through all the lines and process one line at from top to bottom
+   while (line = file.gets.chomp)
+      case line[0]
+        when "FoodItem"
+          #Create new item add it to item set
+        when "Warehouse"
+          #Create new warehouse add it to warehouse set
+        when "Start"
+          #Create startdate
+        when "Request"
+          #stuff
+        when "Recieve"
+          #stuff
+        when "End"
+          #Sstuff
+        else
+          #Gracefully fail
+      end
+   end
   end
 end

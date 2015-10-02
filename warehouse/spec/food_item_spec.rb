4def9d7865c4c126a0a647b55977f27adb760fc7
James Yeates <jimibue@gmail.com>
2015-10-02 13:13:40 -0600

    added initalize tests

--- warehouse/spec/food_item_spec.rb
+++ warehouse/spec/food_item_spec.rb
@@ -1,9 +1,36 @@
-gitrequire 'rspec'
+require 'rspec'
+require_relative '../food_item'
 
 describe 'My behaviour' do
 
-  it 'should do something' do
+  describe 'Basic Tests' do
+    before do
+      @fooditem = FoodItem.new('12345678','12','about')
+    end
 
-    true.should == false
+    describe 'Intialize' do
+      it 'should exist' do
+        expect(@fooditem).not_to be(nil)
+      end
+      it 'should be type FoodItem' do
+        expect(@fooditem.class).to be(FoodItem)
+      end
+      it 'should have a string upc instance variable' do
+        expect(@fooditem.upc).to eq('12345678')
+      end
+
+      it 'should have a integer shelf_life instance variable' do
+        expect(@fooditem.shelf_life).to eq 12
+      end
+      it 'should have a string description nstance variable' do
+        expect(@fooditem.description).to eq 'about'
+      end
+
+
+
+    end
+
   end
+
+
 end

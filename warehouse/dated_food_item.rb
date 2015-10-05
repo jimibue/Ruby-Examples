2f5ab716f4285e9e4d44878506763fc312512819
James Yeates <jimibue@gmail.com>
2015-10-05 16:51:28 -0600

    added expire date instance variable and tests

--- warehouse/dated_food_item.rb
+++ warehouse/dated_food_item.rb
@@ -1,3 +1,21 @@
+require 'date'
 class DatedFoodItem < FoodItem
+  attr_reader :date, :expire_date
 
+  def initialize(upc, shelf_life, description,date)
+    super(upc,shelf_life,description)
+    arr =date.split("/")
+    year = Integer arr[2]
+    month = Integer arr[1]
+    day = Integer arr[0]
+
+    @date = Date.new(year,month,day)
+    @expire_date = @date + Integer(shelf_life)
+    puts @expire_date
+  end
+
+  def get_expire_integer
+    #%Y%_m%_d
+    Integer @expire_date.strftime('%Y%_d%_m').gsub(' ' , '0')
+  end
 end
--- warehouse/spec/dated_food_item_spec.rb
+++ warehouse/spec/dated_food_item_spec.rb
@@ -1,9 +1,72 @@
 require 'rspec'
+require 'spec_helper'
+require_relative '../food_item'
+require_relative '../dated_food_item'
 
 describe 'My behaviour' do
 
-  it 'should do something' do
+  describe 'Basic Tests' do
+    before do
+      @fooditem = DatedFoodItem.new('12345678','12','about','05/01/2010')
+      @fooditem1 = DatedFoodItem.new('12345678','12','about','05/01/2010')
+      @fooditem1a = DatedFoodItem.new('12345678','12','about','05/02/2010')
+      @fooditem2 = DatedFoodItem.new('02345678','12','about','05/02/2010')
 
-    true.should == false
+    end
+
+    describe 'Intialize' do
+      it 'should exist' do
+        expect(@fooditem).not_to be(nil)
+      end
+      it 'should be type FoodItem' do
+        expect(@fooditem.class).to be(DatedFoodItem)
+      end
+      it 'should have a string upc instance variable' do
+        expect(@fooditem.upc).to eq('12345678')
+      end
+
+      it 'should have a integer shelf_life instance variable' do
+        expect(@fooditem.shelf_life).to eq 12
+      end
+      it 'should have a string description nstance variable' do
+        expect(@fooditem.description).to eq 'about'
+      end
+
+      it 'should equal the same item' do
+        expect(@fooditem.== @fooditem1).to be true
+
+      end
+
+      it 'should equal the same item' do
+        expect(@fooditem2.<= @fooditem1).to be true
+
+      end
+
+      it 'two fooditems with different dates should not be equal' do
+        expect(@fooditem1.== @fooditem1a).to be false
+
+      end
+
+      it 'should equal the same item' do
+        expect(@fooditem.to_s).to eq("UPC: 12345678 SHELF LIFE: 12 DESCRIPTION: about")
+
+      end
+
+      it 'should have the correct creation date' do
+       # expect(@fooditem.date).to eq("x")
+
+      end
+
+      it 'should have the correct expire date' do
+        expect(@fooditem.get_expire_integer).to eq(20100513)
+
+      end
+
+
+
+    end
+
   end
+
+
 end

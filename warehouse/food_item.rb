9f85b56ad855ca2011a76b697b320177038ed5de
James Yeates <jimibue@gmail.com>
2015-10-05 10:23:12 -0600

    added ==

--- warehouse/food_item.rb
+++ warehouse/food_item.rb
@@ -1,4 +1,5 @@
 class FoodItem
+  include Comparable # for <=>
   attr_reader :upc, :shelf_life, :description
 
   def initialize(upc, shelf_life, description)
@@ -6,4 +7,26 @@ class FoodItem
     @shelf_life =Integer(shelf_life)
     @description = description
   end
+
+  #this is not necessary and could be done with ==
+  #but was added for demonstration
+  def <=>(other)
+    if self.upc < other.upc
+      -1
+    elsif self.upc > other.upc
+      1
+    else
+      0
+    end
+  end
+
+  def ==(other)
+    #self.upc == other.upc
+    true
+  end
+
+  def to_s
+    "UPC: #{self.upc} SHELF LIFE: #{self.shelf_life} DESCRIPTION: #{self.description}"
+  end
 end
+

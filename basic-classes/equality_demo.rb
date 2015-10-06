eddd472a54a1415907bdd4d29f3a77b213c345ca
James Yeates <jimibue@gmail.com>
2015-10-06 08:24:14 -0600

    added eql? and hash

--- basic-classes/equality_demo.rb
+++ basic-classes/equality_demo.rb
@@ -1,10 +1,26 @@
 class Ex
   def initialize(name, amount)
     @name = name
-    @quantity = quantity
+    @quantity = amount
   end
 
   def to_s
     "Name: #{@name} Amount:#{@quantity}"
+  end
+
+  ##one method for checking for equality and is slow
+  def ==(other)
+    self.name == other.name && self.quantity == other.quantity
+  end
+
+  #hash is faster way to see if objects are equal
+  # ^ is the binary xor, this ensures when state of object is changed so is the hash code
+  def hash
+    self.name.hash ^ self.quantity.hash
+  end
+
+  #eq? is usually the same as == and is needed to be defined when == is overwritten
+  def eql?(other)
+    self == other
   end
 end
